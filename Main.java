60d036d34ea19d69ed19f330a8b6f07596f92935
HarHarHar123 <pnguyen2446@student.pps.net>
2025-03-08 00:55:06 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,75 +1,100 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-
-
         Scanner input = new Scanner(System.in);
-        double nums = 0;
-        double total = 0;
-        ArrayList<Double> list = new ArrayList<Double>();
-        while (nums != -1) {
-            System.out.println("Click -1 to finish putting the grade");
-            System.out.println("Click -2 to remove the grade that you want to remove");
-            System.out.println("enter your grade:");
-            double num = input.nextInt();
-            nums = num;
-            if ( num == -2){
-                System.out.println("in order from 1 to..., which number you want to remove?");
-                System.out.println("Grade list: "+list);
-                int pick =  input.nextInt();
-                if(pick < list.size() && pick != 0){
-                    System.out.println(list.get(pick-1)+ " been removed");
-                    list.remove(pick-1);
+        System.out.println("Pick difficulty: Easy(e), Normal(n), Hard(h)");
+        String input1 = input.nextLine();
 
-                }else{
-                    System.out.println("undefined");
+        String[] easy = {"Which planet is known as the Red Planet? ", "How many kings on a chess board? ", "What is our galaxy named? "};
+        ArrayList<String> easyList = new ArrayList<>();
+        String[] easyAnswer = {"1) Mars, 2) Saturn, 3) Venus, 4) Jupiter", "1) 2, 2) 3, 3) 4, 4) 1", "1) Milk Bear, 2) Milky Moo, 3) Milky Baby, 4) Milky Way"};
+        ArrayList<String> easyAnswerList = new ArrayList<>();
+
+        String[] normal = {"Who painted the Mona Lisa?", "Which gas makes up most of the Earth's atmosphere?", "In which year did the Titanic sink?"};
+        ArrayList<String> normalList = new ArrayList<>();
+        String[] normalAnswer = {"1) Leonardo da Vinci, 2) Pablo Picasso, 3) Vincent van Gogh, 4) Claude Monet", "1) Nitrogen, 2) Oxygen, 3) Carbon Dioxide, 4) Hydrogen", "1) 1900, 2) 1912, 3) 1925, 4) 1930"};
+        ArrayList<String> normalAnswerList = new ArrayList<>();
+
+        String[] hard = {"What is the chemical formula for methane?", "Who was the first woman to win a Nobel Prize?", "What is the capital city of Australia?"};
+        ArrayList<String> hardList = new ArrayList<>();
+        String[] hardAnswer = {"1) CH4, 2) C2H6, 3) CO2, 4) CH3", "1) Marie Curie, 2) Ada Lovelace, 3) Rosalind Franklin, 4) Elizabeth Blackwell", "1) Sydney, 2) Melbourne, 3) Canberra, 4) Adelaide"};
+        ArrayList<String> hardAnswerList = new ArrayList<>();
+
+        for (String question : easy) { easyList.add(question); }
+        for (String question1 : easyAnswer) { easyAnswerList.add(question1); }
+        for (String question : normal) { normalList.add(question); }
+        for (String question1 : normalAnswer) { normalAnswerList.add(question1); }
+        for (String question : hard) { hardList.add(question); }
+        for (String question1 : hardAnswer) { hardAnswerList.add(question1); }
+
+        int point = 0;
+
+        while (easyList.size() != 0 || normalList.size() != 0 || hardList.size() != 0) {
+            if (input1.equals("e") && easyList.size() != 0) {
+                int easyRan = (int) (Math.random() * easyList.size());
+                System.out.println(easyList.get(easyRan));
+                System.out.println(easyAnswerList.get(easyRan));
+                String easyList1 = easyList.remove(easyRan);
+                String easyAnswerList1 = easyAnswerList.remove(easyRan);
+                int easyInput = input.nextInt();
+                if (easyList1.equals("Which planet is known as the Red Planet? ") && easyInput == 1) {
+                    System.out.println("Correct, the answer is Mars");
+                    point++;
+                } else if (easyList1.equals("How many kings on a chess board? ") && easyInput == 1) {
+                    System.out.println("Correct, the answer is 2");
+                    point++;
+                } else if (easyList1.equals("What is our galaxy named? ") && easyInput == 4) {
+                    System.out.println("Correct, the answer is Milky Way");
+                    point++;
+                } else {
+                    System.out.println("Wrong");
                 }
+            } else if (input1.equals("n") && normalList.size() != 0) {
+                int normalRan = (int) (Math.random() * normalList.size());
+                System.out.println(normalList.get(normalRan));
+                System.out.println(normalAnswerList.get(normalRan));
+                String normalList1 = normalList.remove(normalRan);
+                String normalAnswerList1 = normalAnswerList.remove(normalRan);
+                int normalInput = input.nextInt();
+                if (normalList1.equals("Who painted the Mona Lisa?") && normalInput == 1) {
+                    System.out.println("Correct, the answer is Leonardo da Vinci");
+                    point++;
+                } else if (normalList1.equals("Which gas makes up most of the Earth's atmosphere?") && normalInput == 1) {
+                    System.out.println("Correct, the answer is Nitrogen");
+                    point++;
+                } else if (normalList1.equals("In which year did the Titanic sink?") && normalInput == 2) {
+                    System.out.println("Correct, the answer is 1912");
+                    point++;
+                } else {
+                    System.out.println("Wrong");
+                }
+            } else if (input1.equals("h") && hardList.size() != 0) {
+                int hardRan = (int) (Math.random() * hardList.size());
+                System.out.println(hardList.get(hardRan));
+                System.out.println(hardAnswerList.get(hardRan));
+                String hardList1 = hardList.remove(hardRan);
+                String hardAnswerList1 = hardAnswerList.remove(hardRan);
+                int hardInput = input.nextInt();
+                if (hardList1.equals("What is the chemical formula for methane?") && hardInput == 1) {
+                    System.out.println("Correct, the answer is CH4");
+                    point++;
+                } else if (hardList1.equals("Who was the first woman to win a Nobel Prize?") && hardInput == 1) {
+                    System.out.println("Correct, the answer is Marie Curie");
+                    point++;
+                } else if (hardList1.equals("What is the capital city of Australia?") && hardInput == 3) {
+                    System.out.println("Correct, the answer is Canberra");
+                    point++;
+                } else {
+                    System.out.println("Wrong");
+                }
+            } else {
+                break;
             }
-            if (num >= 0 && num <= 100) {
-                list.add(num);
-
-
-            }
-            System.out.println("Grade list: "+list);
-
-
         }
-        for (int i = 0; i < list.size(); i++) {
-            for (int e = 0; e < list.size(); e++) {
-                total += list.get(e);
-            }
-            double check = list.get(i);
-
-
-        }
-        double mean = (double) total / list.size() / list.size();
-        System.out.println("Mean grade: " + mean+"%");
-        //for (int i = 0; i < list.size(); i++) {
-          //  if (list.get(i) < mean) {
-           //     System.out.println("Grade of " + list.get(i) + " below avr");
-         //   }
-       // }
-        double largest = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if ( list.get(i)> largest){
-                largest = list.get(i);
-            }
-        }
-        System.out.println("Highest grade is: "+ largest+"%");
-        double lowest = list.get(0);
-        for (int i = 1; i < list.size(); i++) {
-            if ( list.get(i)< lowest){
-                lowest = list.get(i);
-            }
-        }
-        System.out.println("Lowest grade is: "+ lowest+"%");
-        for (int i = 0; i < list.size(); i++) {
-        if(list.get(i)>= 60){
-            System.out.println("Pass: "+ list.get(i)+"%");
-        }else if(list.get(i)< 60){
-            System.out.println("Failed: "+ list.get(i)+"%");
-        }
-        }
+        System.out.println("You got " + point + " correct!");
     }
 }
+
+
